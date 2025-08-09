0da2a823d9b0bf6c8236f4f313683d9f29d241b5
HP <HP@LAPTOP-G30UVCE8>
2025-08-09 13:53:09 +0530

    SpringBootProject

---
+++ customerservices/src/main/java/com/tnsif/customerservice/CustomerservicesApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerservicesApplication.class, args);
+	}
+
+}
